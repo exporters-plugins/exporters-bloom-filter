f9c38f5dbb17570e71b9bc5a602a0fdd6f897824
bbotella <contacto@bernardobotella.com>
2016-06-24 13:32:34 +0200

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'exporters_bloom_filter',
-    version = '0.1.4',
+    version = '0.1.5',
     author = 'Bernardo Botella',
     author_email = 'contacto@bernardobotella.com',
     packages = find_packages(exclude=['tests']),
