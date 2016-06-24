da815778d2f18fc7b5cb852e5421fd16c69e6bbb
bbotella <contacto@bernardobotella.com>
2016-06-24 13:11:21 +0200

    Bump version: 0.1.0 → 0.1.1

---
+++ setup.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from setuptools import setup, find_packages
+
+setup(
+    name = 'exporters bloom filter',
+    version = '0.1.1',
+    author = 'Bernardo Botella',
+    author_email = 'contacto@bernardobotella.com',
+    packages = find_packages(exclude=['tests']),
+    install_requires = ['exporters', 'pybloom'],
+)
